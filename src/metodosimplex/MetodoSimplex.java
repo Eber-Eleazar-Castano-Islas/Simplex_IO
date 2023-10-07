e5cb4884309c70b76d3c4aa6d31c155d04d73c56
Karina <Karina@Kiel>
2023-10-07 13:30:41 -0500

    Uploading interface

---
+++ src/metodosimplex/MetodoSimplex.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package metodosimplex;
+
+import formsR.mainC;
+
+/**
+ *
+ * @author karam
+ */
+public class MetodoSimplex {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        mainC mC=new mainC();
+        mC.setVisible(true);
+    }
+    
+}
